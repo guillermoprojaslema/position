641a312e2f3d9b2c146c7b2d66e2c848d4fbfff2
Guillermo Rojas <guillermorojaslema@gmail.com>
2021-09-15 02:04:09 -0300

    Main page, It has no functionalities

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,61 +13,6 @@
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
-    <!-- Styles -->
-    {{--<style>--}}
-    {{--html, body {--}}
-    {{--background-color: #fff;--}}
-    {{--color: #636b6f;--}}
-    {{--font-family: 'Nunito', sans-serif;--}}
-    {{--font-weight: 200;--}}
-    {{--height: 100vh;--}}
-    {{--margin: 0;--}}
-    {{--}--}}
-
-    {{--.full-height {--}}
-    {{--height: 100vh;--}}
-    {{--}--}}
-
-    {{--.flex-center {--}}
-    {{--align-items: center;--}}
-    {{--display: flex;--}}
-    {{--justify-content: center;--}}
-    {{--}--}}
-
-    {{--.position-ref {--}}
-    {{--position: relative;--}}
-    {{--}--}}
-
-    {{--.top-right {--}}
-    {{--position: absolute;--}}
-    {{--right: 10px;--}}
-    {{--top: 18px;--}}
-    {{--}--}}
-
-    {{--.content {--}}
-    {{--text-align: center;--}}
-    {{--}--}}
-
-    {{--.title {--}}
-    {{--font-size: 84px;--}}
-    {{--}--}}
-
-    {{--.links > a {--}}
-    {{--color: #636b6f;--}}
-    {{--padding: 0 25px;--}}
-    {{--font-size: 13px;--}}
-    {{--font-weight: 600;--}}
-    {{--letter-spacing: .1rem;--}}
-    {{--text-decoration: none;--}}
-    {{--text-transform: uppercase;--}}
-    {{--}--}}
-
-    {{--.m-b-md {--}}
-    {{--margin-bottom: 30px;--}}
-    {{--}--}}
-    {{--</style>--}}
-
-
 </head>
 <body class="container">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
