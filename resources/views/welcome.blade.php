1f32c832e68cac52f8ed9a9eff9d40530f7579b2
Guillermo Rojas <guillermorojaslema@gmail.com>
2021-09-15 02:03:41 -0300

    Main page, It has no functionalities

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,101 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Position</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Styles -->
+    {{--<style>--}}
+    {{--html, body {--}}
+    {{--background-color: #fff;--}}
+    {{--color: #636b6f;--}}
+    {{--font-family: 'Nunito', sans-serif;--}}
+    {{--font-weight: 200;--}}
+    {{--height: 100vh;--}}
+    {{--margin: 0;--}}
+    {{--}--}}
 
-            .position-ref {
-                position: relative;
-            }
+    {{--.full-height {--}}
+    {{--height: 100vh;--}}
+    {{--}--}}
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    {{--.flex-center {--}}
+    {{--align-items: center;--}}
+    {{--display: flex;--}}
+    {{--justify-content: center;--}}
+    {{--}--}}
 
-            .content {
-                text-align: center;
-            }
+    {{--.position-ref {--}}
+    {{--position: relative;--}}
+    {{--}--}}
 
-            .title {
-                font-size: 84px;
-            }
+    {{--.top-right {--}}
+    {{--position: absolute;--}}
+    {{--right: 10px;--}}
+    {{--top: 18px;--}}
+    {{--}--}}
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    {{--.content {--}}
+    {{--text-align: center;--}}
+    {{--}--}}
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    {{--.title {--}}
+    {{--font-size: 84px;--}}
+    {{--}--}}
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+    {{--.links > a {--}}
+    {{--color: #636b6f;--}}
+    {{--padding: 0 25px;--}}
+    {{--font-size: 13px;--}}
+    {{--font-weight: 600;--}}
+    {{--letter-spacing: .1rem;--}}
+    {{--text-decoration: none;--}}
+    {{--text-transform: uppercase;--}}
+    {{--}--}}
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+    {{--.m-b-md {--}}
+    {{--margin-bottom: 30px;--}}
+    {{--}--}}
+    {{--</style>--}}
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+
+</head>
+<body class="container">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+<div class="flex-center position-ref full-height">
+    <div class="content">
+
+        <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Position</h1>
+                <p class="col-md-8 fs-4">Escriba una URL y te diremos cuántas imágenes tiene el sitio y cuántos archivos
+                    css</p>
+
+                <form action="#" class="row g-3" method="get">
+                    <div class="col-auto">
+                        <label for="input" class="visually-hidden">Url</label>
+                        <input type="text" class="form-control" id="input" placeholder="Ej: www.google.cl">
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-primary mb-3">Averiguar</button>
+                    </div>
+                </form>
             </div>
         </div>
-    </body>
+
+    </div>
+</div>
+</body>
 </html>
